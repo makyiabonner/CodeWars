7a085a0fa2f17e42b8a552b1a142c05a8046c4ae
makyia bonner <makyiabonner@gmail.com>
2023-03-02 20:18:08 -0500

    kata solved

--- codewars/index.js
+++ codewars/index.js
@@ -3,3 +3,13 @@ function duplicateEncode(word) {
     word = word.toLowerCase();
     return word.replace(/./g, m => word.indexOf(m) == word.lastIndexOf(m) ? '(' : ')');
   }
+
+//Sort the odd
+function sortArray(array) {
+  // filter odd numbers and sort them
+  // loop through array
+  // if array[num] is even, shift odd number infront of even number
+  //    else continue array loop
+  let odd = array.filter((x) => x %2).sort((a,b) => a-b)
+  return (array.map(num => num %2 ? odd.shift() : num))
+}
