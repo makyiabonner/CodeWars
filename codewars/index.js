d8598ff75d7507de8d4833c7e7a3f32db182d651
makyia bonner <makyiabonner@gmail.com>
2023-03-07 12:01:02 -0500

    6kyu solved

--- codewars/index.js
+++ codewars/index.js
@@ -144,3 +144,15 @@ function rentalCarCost(d) {
   
   return cost;
 }
+
+//Who likes it
+function likes(names) {
+  // Use switch statement
+  switch(names.length){
+      case 0 : return `no one likes this`
+      case 1 : return `${names[0]} likes this`
+      case 2 : return `${names[0]} and ${names[1]} like this`
+      case 3 : return `${names[0]}, ${names[1]} and ${names[2]} like this`
+      default: return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`
+  }
+}
