6a7b1fc978b8653ed78f986b6c045da4379ad668
makyia bonner <makyiabonner@gmail.com>
2023-11-03 16:54:20 -0400

    #Solved python question: Calculate BMI

--- codewars/index.py
+++ codewars/index.py
@@ -32,4 +32,17 @@ def quarter_of(month):
 
 def sum_array(a):
     return sum(a)
-#Solved python question: Sum Arrays
+
+def bmi(weight, height):
+    bmi = weight / pow(height, 2)
+    if bmi <= 18.5 :
+        return 'Underweight'
+    elif bmi <= 25.0 :
+        return 'Normal'
+    elif bmi <= 30.0 :
+        return 'Overweight'
+    else: return 'Obese'
+
+print(bmi(208,1.8))
+print(bmi(220,1.4))
+#Solved python question: Calculate BMI
