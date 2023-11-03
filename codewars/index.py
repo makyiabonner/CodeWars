9b8036ff0ecbbfafaaf67e1073a276478d33dd15
makyia bonner <makyiabonner@gmail.com>
2023-11-02 23:25:20 -0400

    Solved python question: Century From Year

--- codewars/index.py
+++ codewars/index.py
@@ -7,4 +7,17 @@ def repeat_str(repeat, string):
 def find_average(numbers):
     return sum(numbers) / len(numbers) if len(numbers) >= 1 else 0
 
-#Solved python question:
+def century(years):
+    century = 0
+    if years >= 1000 :
+        if int(str(years)[-2:]):
+            century = int(str(years)[:2]) + 1
+        else: century = int(str(years)[:2])
+    elif years >= 100 :
+        if int(str(years)[-2:]):
+            century = int(str(years)[:1]) + 1
+        else: century = int(str(years)[:1])
+    else : century = 1
+    return century
+    
+#Solved python question: Century From Year
