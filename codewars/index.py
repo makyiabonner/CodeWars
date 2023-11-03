a7bbfc2813d47f1d33270e45afc3d0480a57d7a5
makyia bonner <makyiabonner@gmail.com>
2023-11-02 23:34:19 -0400

    Solved python question: Quarter of the Year

--- codewars/index.py
+++ codewars/index.py
@@ -19,5 +19,14 @@ def century(years):
         else: century = int(str(years)[:1])
     else : century = 1
     return century
-    
-#Solved python question: Century From Year
+
+def quarter_of(month):
+    if month in range(1,4):
+        return 1
+    elif month in range(4,6):
+        return 2
+    elif month in range(7,10):
+        return 3
+    elif month in range(10,13):
+        return 4
+#Solved python question: Quarter of the Year
